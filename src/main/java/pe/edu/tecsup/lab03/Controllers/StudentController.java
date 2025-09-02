6625963ff8494acc7969ab55665ef57de8d0d250
Kreissys <carlos.llano@tecsup.edu.pe>
2025-09-01 18:54:24 -0600

    second Commit

--- src/main/java/pe/edu/tecsup/lab03/Controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/Controllers/StudentController.java
@@ -1,4 +1,9 @@
 package pe.edu.tecsup.lab03.Controllers;
 
 public class StudentController {
+
+    // Comentario: Esta es mi clase controlador Gaaaaaaaaaaaaaaaaaaaaa
+
+    //Modificacion Carlos
+
 }
